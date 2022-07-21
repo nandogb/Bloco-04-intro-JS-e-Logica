13a57c7f317e7f6e9ef54f32617e0ed9a25bb58e
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-07-21 15:46:28 -0300

    dia 02

---
+++ dia02.js
@@ -0,0 +1,61 @@
+//! arrays e listas ##############
+
+// let pizzas = [
+//   "Portuguesa",
+//   "Charque com Catupiry",
+//   "Calabresa",
+//   "Lombo com Catupiry",
+//   "Mussarela",
+//   "Palmito",
+//   "Marguerita",
+// ];
+
+// pizzas.push("Rúcula com Tomate Seco");
+
+// for (let i = 0; i < pizzas.length; i++) {
+//   console.log(i + 1 + " " + pizzas[i]);
+// }
+
+//! estruturas de repitção com FOR ##########
+
+// let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (let contador = 0; contador < numeros.length; contador++) {
+//   let tabuada = 2 * numeros[contador];
+//   console.log(tabuada);
+// }
+
+//! estruturas de repetião OF ################
+
+// let comida = "Parmegiana";
+
+// for (const isso of comida) {
+//     console.log(isso);
+// }
+
+// let comidas = ["pizza", "dog", "pão de alho", "bolo de rolo"];
+
+// for (const alimentos of comidas) {
+//     console.log(alimentos)
+
+// }
+
+//! estruturas de repetição while/do ###############
+
+// var resultado = []
+// var i = 0
+
+// while (i<5) {
+//     i += 1
+//     console.log(i)
+// }
+
+// do {
+//     i += 1
+// } while (i < 5);
+// console.log(i)
+
+for(let index = 0; index < 10; index+=1 ){
+    console.log(index);
+  }
+  
