412e8bb7607d1098e7bbc9638325226302ada34d
Fernando Barreto <fernando.gbarreto@hotmail.com>
2022-07-25 16:18:17 -0300

    dia 04

---
+++ dia04.js
@@ -0,0 +1,85 @@
+//! Objetos e funções
+
+// let pizza = {
+//   sabor: "Portuguesa",
+//   tamanho: "Grande",
+//   ingredientes: ["cebola", "presunto", "ovo", "azeitonas pretas"],
+// };
+// console.log(pizza.sabor);
+
+// console.log(pizza["ingredientes"]);
+
+// console.table(pizza)
+
+//? array de objetos
+
+// let moradores = [
+//     {
+//       nome: 'Luiza',
+//       sobrenome: 'Guimarães',
+//       andar: 10,
+//       apartamento: 1005,
+//     },
+//     {
+//       nome: 'William',
+//       sobrenome: 'Albuquerque',
+//       andar: 5,
+//       apartamento: 502,
+//     },
+//     {
+//       nome: 'Murilo',
+//       sobrenome: 'Ferraz',
+//       andar: 8,
+//       apartamento: 804,
+//     },
+//     {
+//       nome: 'Zoey',
+//       sobrenome: 'Brooks',
+//       andar: 1,
+//       apartamento: 101,
+//     },
+//   ];
+
+//   let primeiroMorador = moradores[0];
+//   console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+//   console.log(primeiroMorador['andar']); // 10
+
+//   let ultimoMorador = moradores[moradores.length - 1];
+//   console.table(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+//   console.log(ultimoMorador.nome); // Zoey
+
+//! For/in
+
+// let pizza = {
+//       sabor: "Portuguesa",
+//       tamanho: "Grande",
+//       ingredientes: ["cebola", "presunto", "ovo", "azeitonas pretas"],
+//     };
+
+//     for (let key in pizza) {
+//         console.log(pizza[key]);
+//     }
+
+//! For/Of
+
+// let pizza = {
+//       sabor: "Portuguesa",
+//       tamanho: "Grande",
+//       ingredientes: ["cebola", "presunto", "ovo", "azeitonas pretas"],
+//     };
+
+//     for (let key of pizza.ingredientes) {
+//         console.log(key);
+//     }
+
+// let numerosMegaSena = [1, 25, 32, 44, 51, 60];
+
+// for (let key in numerosMegaSena) {
+//   console.log(key, numerosMegaSena[key]);
+// }
+
+//! funções
+
+function name(params) {
+    
+}
